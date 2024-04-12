a53344883acf1e233cc553c1a4bdd907532bb9a9
Research Scientist <8367366+ResearchScientist@users.noreply.github.com>
2024-04-12 22:38:08 +0000

    [db] define schema

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,26 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column } from 'astro:db';
 
-// https://astro.build/db/config
+// Resources //
+// https://docs.astro.build/en/guides/astro-db/
+// https://docs.astro.build/en/guides/integrations-guide/db/
+
+const Users = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true}),
+    username: column.text({ optional: false, unique: true }),
+  }
+});
+
+const Goals = defineTable({
+    columns: {
+        id: column.number({ primaryKey: true }),
+        description: column.text({ optional: false }),
+        completed: column.boolean({ default: false }),
+        user: column.number({ references: () => Users.columns.id }),
+    }
+});
+
 export default defineDb({
-  tables: {}
+  // tables: { Goals }
+  tables: { Users, Goals }
 });

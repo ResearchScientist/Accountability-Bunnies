3b73e5f373a58d3af5ef13ff25231e10b2362b5a
Ruben A Sanchez <8367366+ResearchScientist@users.noreply.github.com>
2024-04-22 23:55:34 +0000

    [goals] test add and delete

--- src/scripts/goals.ts
+++ src/scripts/goals.ts
@@ -4,16 +4,33 @@ const goalsSection = document.querySelector('#goals-section');
 const deleteGoalButton = document.querySelector('#delete-goal-button');
 
 addGoalButton.addEventListener('click',addGoal);
-deleteGoalButton.addEventListener('click',deleteGoal);
+// deleteGoalButton.addEventListener('click',deleteGoal);
 
 function addGoal(e) {
     e.preventDefault();
-    console.log('add goal');
     let goalText = inputGoal.value;
     console.log(goalText);
+    if (goalText) {
+        let li = document.createElement('li');
+        let img = document.createElement('img');
+        let btn = document.createElement('button');
+        btn.addEventListener('click',deleteGoal);
+        li.classList.add('goal-item');
+        img.classList.add('tikbox');
+        btn.textContent = "x";
+        btn.classList.add('delete-goal-button');
+        img.src = "tikbox.svg";
+        li.appendChild(img);
+        li.textContent = goalText;
+        li.appendChild(btn);
+        goalsSection.appendChild(li);
+    }
+    inputGoal.value = "";
 }
 
 
-function deleteGoal() {
+function deleteGoal(event) {
     console.log('delete goal');
+    let goal = event.target.parentElement;
+    goalsSection.removeChild(goal);
 }

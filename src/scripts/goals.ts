9d7ad28e8cb32c0aedc0c7969120f0324de61efd
Ruben A Sanchez <8367366+ResearchScientist@users.noreply.github.com>
2024-04-23 00:45:26 +0000

    [goals] add dynamic entries

--- src/scripts/goals.ts
+++ src/scripts/goals.ts
@@ -4,25 +4,24 @@ const goalsSection = document.querySelector('#goals-section');
 const deleteGoalButton = document.querySelector('#delete-goal-button');
 
 addGoalButton.addEventListener('click',addGoal);
-// deleteGoalButton.addEventListener('click',deleteGoal);
 
 function addGoal(e) {
     e.preventDefault();
     let goalText = inputGoal.value;
-    console.log(goalText);
     if (goalText) {
         let li = document.createElement('li');
         let img = document.createElement('img');
+        let div = document.createElement('div');
         let btn = document.createElement('button');
         btn.addEventListener('click',deleteGoal);
         li.classList.add('goal-item');
+        img.src = "tikbox.svg";
         img.classList.add('tikbox');
+        div.textContent = goalText;
+        div.classList.add('goal-text');
         btn.textContent = "x";
         btn.classList.add('delete-goal-button');
-        img.src = "tikbox.svg";
-        li.appendChild(img);
-        li.textContent = goalText;
-        li.appendChild(btn);
+        li.append(img,div,btn);
         goalsSection.appendChild(li);
     }
     inputGoal.value = "";

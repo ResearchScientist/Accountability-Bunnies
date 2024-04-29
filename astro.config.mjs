f0f78e8c0e7f957991bc0bc6116b00ef012dea48
Ruben A Sanchez <8367366+ResearchScientist@users.noreply.github.com>
2024-04-29 19:00:43 +0000

    [test] remove netlify integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import db from "@astrojs/db";
-import netlify from "@astrojs/netlify";
+// import netlify from "@astrojs/netlify";
 
 export default defineConfig({
   integrations: [db()],
   output: "static",
-  adapter: netlify()
+  // adapter: netlify()
 });

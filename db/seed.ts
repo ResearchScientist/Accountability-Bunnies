a254c9100a7e92e2d2a39b2a977010f96fce409d
Ruben A Sanchez <8367366+ResearchScientist@users.noreply.github.com>
2024-05-01 16:52:02 +0000

    [db] add table to import

--- db/seed.ts
+++ db/seed.ts
@@ -1,16 +1,22 @@
-import { db, Goals } from 'astro:db';
+import { db, Goals, Bunnies } from 'astro:db';
 
 export default async function() {
     await db.insert(Goals).values([
         {
             id: 1,
-            description: 'eat a cookie',
+            description: 'bake a cookie',
             completed: true,
         },
         {
             id: 2,
             description: 'hug a bear',
             completed: false,
+        }
+    ]),
+    await db.insert(Bunnies).values([
+        {
+            id: 10,
+            totalBunnies: 5,
         }
     ])
 }

3a80773526b3641ab4d85ba61dd895198e8fa808
Ruben A Sanchez <8367366+ResearchScientist@users.noreply.github.com>
2024-05-12 18:32:01 +0000

    [db] bunnies : add total bunnies const

--- .netlify/functions-internal/ssr/chunks/generic_B0feq_Um.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/generic_Kzoxrf0b.mjs');
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/index_C2oC1UuZ.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_DoDK28N7.mjs');
-
-export { page };

7f4f28b042b7021e7bbd5aa69f26c96d5ef82c45
Ruben A Sanchez <8367366+ResearchScientist@users.noreply.github.com>
2024-05-13 20:15:18 +0000

    [test] server

--- src/pages/api/goals/index.ts
+++ src/pages/api/goals/index.ts
@@ -3,6 +3,7 @@ import { db, Goals } from 'astro:db';
 
 export const GET: APIRoute = async (ctx) => {
     try {
+      console.log('did it try to GET');
         const allGoals = await db.select().from(Goals);
         return new Response(JSON.stringify(allGoals), {
             status: 200,
@@ -12,18 +13,21 @@ export const GET: APIRoute = async (ctx) => {
         });
     }
     catch (error) {
+      console.log(`error happened ${error.message}`);
         return new Response(`Error: ${error.message}`,{ status: 500 });
     }
 };
 
 export const POST: APIRoute = async ({ request }) => {
   try {
+    console.log('did it try to POST');
       const data = await request.json();
       const newGoal = { ...data, completed: false };
       await db.insert(Goals).values(newGoal);
       return new Response(null, { status: 201 });
   }
   catch (error) {
+     console.log(`post error ${error.message}`);
       return new Response(`Error: ${error.message}`,{ status: 500 });
   }
 };

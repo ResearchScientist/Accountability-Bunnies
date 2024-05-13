4f2cd880ece802820c650e3e203d942ddb52c5c1
Ruben A Sanchez <8367366+ResearchScientist@users.noreply.github.com>
2024-05-13 19:25:28 +0000

    [db] logic : add get request

--- src/pages/api/goals/index.ts
+++ src/pages/api/goals/index.ts
@@ -1,10 +1,18 @@
-export async function GET() {
-    return new Response(
-    JSON.stringify({msg: 'hiya'}), {
-      status: 200,
-      headers: {
-        "Content-Type": "application/json"
-      }
+import type { APIRoute } from 'astro';
+import { db, Goals } from 'astro:db';
+
+export const GET: APIRoute = async (ctx) => {
+    try {
+        const allGoals = await db.select().from(Goals);
+        return new Response(JSON.stringify(allGoals), {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        });
     }
-  );
-}
+    catch (error) {
+        return new Response(`Error: ${error.message}`,{ status: 500 });
+    }
+};
+

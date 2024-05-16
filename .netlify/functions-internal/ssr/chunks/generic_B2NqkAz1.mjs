b8785db493c6a0a781869dfdd7a5ba1f1afda4b7
Ruben A Sanchez <8367366+ResearchScientist@users.noreply.github.com>
2024-05-16 18:35:29 +0000

    [dev] update build

---
+++ .netlify/functions-internal/ssr/chunks/generic_B2NqkAz1.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/generic_Cc-kmgPh.mjs');
+
+export { page };
---
+++ .netlify/functions-internal/ssr/chunks/index_ByWHDHZx.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/index_BpJ0VtJz.mjs');
+
+export { page };

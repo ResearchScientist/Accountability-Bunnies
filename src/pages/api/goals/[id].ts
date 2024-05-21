10505e3c0dc3bfa2cc185d53ea754427ffe39aa9
Ruben A Sanchez <8367366+ResearchScientist@users.noreply.github.com>
2024-05-21 19:01:27 +0000

    [db] test patch

--- src/pages/api/goals/[id].ts
+++ src/pages/api/goals/[id].ts
@@ -14,3 +14,20 @@ export const DELETE: APIRoute = async (ctx) => {
         return new Response(`Error: ${error.message}`,{status: 500});
     }
 };
+
+// export const PATCH: APIRoute = async (ctx) => {
+//     const id = Number(ctx.params.id);
+//     const goal = await db.select(Goals).where(eq(Goals.id,id));
+//     const newCompletedValue = goal.completed === 'yes' ? 'no' : 'yes';
+//     if (!id) {
+//         return new Response('goal not found',{ status: 404 });
+//     }
+//     try {
+//         // await db.update(Goals).set({ completed: 'yes' }).where(eq(Goals.id,id));
+//         await db.update(Goals).set({ completed: newCompletedValue }).where(eq(Goals.id,id));
+//     }
+//     catch (error) {
+//         return new Response('somethings not right',{ status: 500 });
+//     }
+//     return new Response('updated completed',{ status: 200 });
+// };

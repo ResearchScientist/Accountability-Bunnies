27a4e4b9b78c06edfcb77bbda12c10b212ae1d9a
Ruben A Sanchez <8367366+ResearchScientist@users.noreply.github.com>
2024-05-21 21:49:10 +0000

    [db] test patch

--- src/pages/api/goals/[id].ts
+++ src/pages/api/goals/[id].ts
@@ -15,19 +15,25 @@ export const DELETE: APIRoute = async (ctx) => {
     }
 };
 
-// export const PATCH: APIRoute = async (ctx) => {
-//     const id = Number(ctx.params.id);
-//     const goal = await db.select(Goals).where(eq(Goals.id,id));
+export const PATCH: APIRoute = async (ctx) => {
+    // const goal = await db.select(Goals).where(eq(Goals.id,id));
 //     const newCompletedValue = goal.completed === 'yes' ? 'no' : 'yes';
-//     if (!id) {
-//         return new Response('goal not found',{ status: 404 });
-//     }
-//     try {
+    try {
+        console.log('trying patch route');
+        const id = Number(ctx.params.id);
+        // const goal = await db.select(Goals).where(eq(Goals.id,id));
+        console.log('the patch id is',id);
+        if (!id) {
+            return new Response('goal id not foundl',{ status: 400 });
+        }
+        console.log('patching going on');
+        console.log('completed status is',ctx.params.completed);
+        return new Response(null, { status: 204 });
 //         // await db.update(Goals).set({ completed: 'yes' }).where(eq(Goals.id,id));
 //         await db.update(Goals).set({ completed: newCompletedValue }).where(eq(Goals.id,id));
-//     }
-//     catch (error) {
-//         return new Response('somethings not right',{ status: 500 });
-//     }
-//     return new Response('updated completed',{ status: 200 });
-// };
+    }
+    catch (error) {
+        console.log('patch not routing');
+        return new Response('somethings not right',{ status: 500 });
+    }
+};

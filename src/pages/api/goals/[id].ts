83eae940a5346a136d9392967193c68993a53507
Ruben A Sanchez <8367366+ResearchScientist@users.noreply.github.com>
2024-05-21 22:33:56 +0000

    [db] test patch

--- src/pages/api/goals/[id].ts
+++ src/pages/api/goals/[id].ts
@@ -21,13 +21,19 @@ export const PATCH: APIRoute = async (ctx) => {
     try {
         console.log('trying patch route');
         const id = Number(ctx.params.id);
-        // const goal = await db.select(Goals).where(eq(Goals.id,id));
         console.log('the patch id is',id);
         if (!id) {
             return new Response('goal id not foundl',{ status: 400 });
         }
         console.log('patching going on');
-        console.log('completed status is',ctx.params.completed);
+        // console.log('completed status is',ctx.params.completed);
+        // const bodyText = await ctx.request.body.text();
+        // const body = JSON.parse(bodyText);
+        // const completed = body.completed;
+
+        // const formData = await ctx.request.formData();
+        // const completed = formData.get('completed');
+        // console.log('completed status is',completed);
         return new Response(null, { status: 204 });
 //         // await db.update(Goals).set({ completed: 'yes' }).where(eq(Goals.id,id));
 //         await db.update(Goals).set({ completed: newCompletedValue }).where(eq(Goals.id,id));

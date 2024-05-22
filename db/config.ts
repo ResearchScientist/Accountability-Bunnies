e1592afbea47996d4979b7ba6f1164ffce8d05fd
Ruben A Sanchez <8367366+ResearchScientist@users.noreply.github.com>
2024-05-22 17:49:35 +0000

    Revert "[db] update schema"
    This reverts commit e0048c04f215f189c46adf801f9ba5e0ef9ce2f6.

--- db/config.ts
+++ db/config.ts
@@ -11,7 +11,7 @@ const Goals = defineTable({
     columns: {
         id: column.number({ primaryKey: true }),
         description: column.text({ optional: false }),
-        completed: column.boolean({ optional: false , default: false }),
+        completed: column.text({ optional: false , default: 'no' }),
         // user: column.number({ references: () => Users.columns.id }),
     }
 });

b2c4bb58e39bc90fdb86e8bbc70b0d5c74c4ebdd
Ruben A Sanchez <8367366+ResearchScientist@users.noreply.github.com>
2024-05-24 00:24:11 +0000

    [db] add completed endpoint

---
+++ src/pages/api/goals/complete.ts
@@ -0,0 +1,21 @@
+import type { APIRoute } from 'astro';
+import { db, Goals } from 'astro:db';
+
+export const GET: APIRoute = async () => {
+    try {
+        const allGoals = await db.select().from(Goals);
+        const completedGoals =  allGoals.filter(goal => goal.completed === 'yes').length;
+        const notcompletedGoals = allGoals.length - completedGoals;
+        return new Response(JSON.stringify({ completedGoals , notcompletedGoals
+    }),
+    {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    });
+    }
+    catch (error) {
+        return new Response(`Error: ${error.message}`,{ status: 500 });
+    }
+};

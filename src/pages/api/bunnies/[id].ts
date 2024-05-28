08498e1b7b8e583afd22e8f9b7882a93bfadfda9
Ruben A Sanchez <8367366+ResearchScientist@users.noreply.github.com>
2024-05-28 22:47:29 +0000

    [db] test

--- src/pages/api/bunnies/[id].ts
+++ src/pages/api/bunnies/[id].ts
@@ -4,14 +4,29 @@ import { db, Bunnies, eq } from 'astro:db';
 export const PATCH: APIRoute = async (ctx) => {
     try {
         const id = Number(ctx.params.id);
-        const body = await ctx.request.json();
+        const { newTotalBunnies,updated } = await ctx.request.json();
         if (!id) {
             return new Response('total id not foundl',{ status: 400 });
         }
-        await db.update(Bunnies).set(body).where(eq(Bunnies.id,id));
+        await db.update(Bunnies).set({ totalBunnies: newTotalBunnies, updated }).where(eq(Bunnies.id,id));
         return new Response('update successful',{ status: 200 });
     }
     catch (error) {
         return new Response('something not right',{ status: 500 });
     }
 }
+
+// export const PATCH: APIRoute = async (ctx) => {
+//     try {
+//         const id = Number(ctx.params.id);
+//         const body = await ctx.request.json();
+//         if (!id) {
+//             return new Response('total id not foundl',{ status: 400 });
+//         }
+//         await db.update(Bunnies).set(body).where(eq(Bunnies.id,id));
+//         return new Response('update successful',{ status: 200 });
+//     }
+//     catch (error) {
+//         return new Response('something not right',{ status: 500 });
+//     }
+// }

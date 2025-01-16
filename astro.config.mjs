b46388fb3e4758ab79b5fd0e97f7b18f3fdf95eb
Ruben A Sanchez <8367366+ResearchScientist@users.noreply.github.com>
2025-01-16 10:58:59 -0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-// import db from "@astrojs/db";
+import db from "@astrojs/db";
 import netlify from "@astrojs/netlify";
 
 export default defineConfig({
-  // integrations: [db()],
+  integrations: [db()],
   output: "server",
   adapter: netlify(),
   security: {
